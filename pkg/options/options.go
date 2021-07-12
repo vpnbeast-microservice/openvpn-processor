00bdce4824f587a56f296d6b8d46530f1b2a57ed
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-07-12 21:42:11 +0300

    move utility functions to utils file

--- pkg/options/options.go
+++ pkg/options/options.go
@@ -3,8 +3,6 @@ package options
 import (
 	"go.uber.org/zap"
 	"openvpn-processor/pkg/logging"
-	"os"
-	"strconv"
 )
 
 var (
@@ -69,31 +67,3 @@ func (opo *OpenvpnProcessorOptions) initOptions() {
 	opo.HealthEndpoint = getStringEnv("HEALTH_ENDPOINT", "/health")
 }
 
-// getStringEnv gets the specific environment variables with default value, returns default value if variable not set
-func getStringEnv(key, defaultValue string) string {
-	value := os.Getenv(key)
-	if len(value) == 0 {
-		return defaultValue
-	}
-	return value
-}
-
-// getIntEnv gets the specific environment variables with default value, returns default value if variable not set
-func getIntEnv(key string, defaultValue int) int {
-	value := os.Getenv(key)
-	if len(value) == 0 {
-		return defaultValue
-	}
-	return ConvertStringToInt(value)
-}
-
-// ConvertStringToInt converts string environment variables to integer values
-func ConvertStringToInt(s string) int {
-	i, err := strconv.Atoi(s)
-	if err != nil {
-		logger.Warn("an error occurred while converting from string to int. Setting it as zero",
-			zap.String("error", err.Error()))
-		i = 0
-	}
-	return i
-}
---
+++ pkg/options/utils.go
@@ -0,0 +1,36 @@
+package options
+
+import (
+	"go.uber.org/zap"
+	"os"
+	"strconv"
+)
+
+// getStringEnv gets the specific environment variables with default value, returns default value if variable not set
+func getStringEnv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return defaultValue
+	}
+	return value
+}
+
+// getIntEnv gets the specific environment variables with default value, returns default value if variable not set
+func getIntEnv(key string, defaultValue int) int {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return defaultValue
+	}
+	return ConvertStringToInt(value)
+}
+
+// ConvertStringToInt converts string environment variables to integer values
+func ConvertStringToInt(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		logger.Warn("an error occurred while converting from string to int. Setting it as zero",
+			zap.String("error", err.Error()))
+		i = 0
+	}
+	return i
+}
